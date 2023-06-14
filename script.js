753a65c2676c3d931516598d2a68e55e318789d0
valdineijunior <valdineidepaulajunior@gmail.com>
2023-06-13 23:52:22 -0300

    implementando primeira versão da calculadora para churrasco

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById('calculatorForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obter os valores selecionados
+    var eventType = document.getElementById('eventType').value;
+    var guestProfile = document.getElementById('guestProfile').value;
+    var sides = document.getElementById('sides').value;
+    var numberOfGuests = document.getElementById('numberOfGuests').value;
+
+    // Calcular a quantidade de carne por pessoa
+    var meatPerGuest = calculateMeatPerGuest(eventType, guestProfile, sides);
+
+    // Calcular a quantidade total de carne
+    var totalMeat = meatPerGuest * numberOfGuests;
+
+    // Exibir os resultados
+    document.getElementById('resultPerGuest').textContent = 'Quantidade por pessoa: ' + meatPerGuest.toFixed(3) + ' kg';
+    document.getElementById('resultTotal').textContent = 'Quantidade total: ' + totalMeat.toFixed(3) + ' kg';
+
+    document.getElementById('result').style.display = 'block';
+});
+
+function calculateMeatPerGuest(eventType, guestProfile, sides) {
+    var baseAmount = 0.3;
+
+    if (eventType === 'lunch') {
+        baseAmount += 0.1
+    }
+    
+    if (eventType === 'unending') {
+        baseAmount += 0.2
+    }
+
+    if (guestProfile === 'adults') {
+        baseAmount += 0.1;
+    }
+
+    if (sides === 'snacks') {
+        baseAmount += 0.1;
+    }
+
+    return baseAmount;
+}
